b83a882585a3cc1f9e13f8d17300273eaafed3a6
Kkim-Genie <kimgenie0409@gmail.com>
2025-05-04 21:30:56 +0900

    feat: get latest newstoday

---
+++ app/api/news/newstoday/latest/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { news } from "@/lib/db/schema/news";
+import { desc, eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const newstodayNews = await db
+      .select()
+      .from(news)
+      .where(eq(news.company, "newstoday"))
+      .orderBy(desc(news.date))
+      .limit(1);
+    return NextResponse.json(newstodayNews[0].date);
+  } catch (error) {
+    console.error("Error fetching resources:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch resources" },
+      { status: 500 }
+    );
+  }
+}
